233ed15a2b923b083e71310210c26ca331746646
Mr nova <mamali.azim@gmail.com>
2016-07-24 19:27:35 +0430

    Update logo.lua

--- plugins/logo.lua
+++ plugins/logo.lua
@@ -1,25 +1,43 @@
-do
-
+local namefa = "کرول"
+local nameen = "[Cc][Rr][Uu][Ee][Ll]"
+local function get_response(text)
+if text:match(nameen) then
+  text = text:gsub(nameen.." ","")
+end
+if text:match(namefa) then
+  text = text:gsub(namefa.." ","")
+end
+local lang = redis:get('simsimi:lang')
+local myurl = 'https://iteam-co.ir/simsimi.php?msg='..URL.escape(text)..'&lang='..lang
+return https.request(myurl)
+end
+local function action_by_reply(extra, success, result)
+if success then
+  local bot = our_id
+  if result.from.peer_id == tonumber(bot) then
+    local msg2 = extra.msg
+  local matn = msg2.text
+    reply_msg(msg2.id, get_response(matn), ok_cb, true)
+  end
+  end
+end
 function run(msg, matches)
-       if not is_sudo(msg) then
-        return "فقط مخصوص سودو می باشد"
-       end
-    local data = load_data(_config.moderation.data)
-      local group_link = data[tostring(msg.to.id)]['settings']['set_link']
-       if not group_link then 
-        return "اول باید لینک جدید ایجاد کنید"
-       end
-         local text = "  🏷لینک ورود به گروه:\n"..group_link
-          send_large_msg('user#id'..msg.from.id, text.."\n", ok_cb, false) send_document(get_receiver(msg), "./logo/sent.webp", ok_cb, false)
-           return "لینک به پیوی شما ارسال شد ℹ️"
-end
-
+local text = msg.text
+ if text:match("[!/#][Ss]etlang (.*)") and is_sudo(msg) then
+  local lang = text:match("[!/#][Ss]etlang (.*)")
+    redis:set('simsimi:lang',lang)
+    return 'Lang Set To '..lang
+  elseif text:match(nameen.." (.*)") then
+  reply_msg(msg.id, get_response(text), ok_cb, true)
+  elseif text:match(namefa.." (.*)") then
+  reply_msg(msg.id, get_response(text), ok_cb, true)
+  elseif msg.reply_id then
+    get_message(msg.reply_id, action_by_reply, {msg=msg})
+  end
+  end
 return {
-  patterns = {
-    "^[/#!]([Ll]inkpv)$"
-    "^[Ll]linkpv$"
-  },
-  run = run
+   patterns = {
+"(.*)"
+},
+   run = run
 }
-
-end
